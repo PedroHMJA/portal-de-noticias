d66bed23d7c033504b554daf8fec57389ef6f1e9
PedroHMJA <pedromayer.alves@outlook.com>
2025-07-01 18:30:39 -0300

    Ajuste no modelo noticia

--- frontend/portal/src/app/model/noticia.ts
+++ frontend/portal/src/app/model/noticia.ts
@@ -1,12 +1,31 @@
-import { Categoria } from "./categoria";
-import { Usuario } from "./usuario";
+import { Categoria } from './categoria';
+import { Usuario } from './usuario';
 
 export class Noticia {
-    id?: number;
-    titulo?: string;
-    corpo?: string;
-    dataPublicacao?: string;
-    imagem_url?: string;
-    categoria?: Categoria;
-    usuario?: Usuario; 
+  id?: number;
+  titulo?: string;
+  corpo?: string;
+  dataPublicacao?: string;
+  imagem_url?: string;
+  categoria?: Categoria;
+  usuario?: Usuario;
+
+  constructor(
+    id?: number,
+    titulo?: string,
+    corpo?: string,
+    dataPublicacao?: Date,
+    imagem_url?: string,
+    categoria?: Categoria,
+    usuario?: Usuario
+  ) {}
+
+  static fromJson(json: any): Noticia {
+    return new Noticia(
+      json.id,
+      json.titulo,
+      json.corpo,
+      new Date(json.dataPublicacao)
+    );
+  }
 }
